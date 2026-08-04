25da21fbdcf79ef7afbedf532d92d83c8be52997
Florian Moser <florian_moser@gmx.at>
2026-08-05 00:57:28 +0200

    Bauviqo Web-Build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4185863110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "51867134" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
